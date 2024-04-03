8143b8d6ad8c4c6be00dad1287ab5777859a8661
sheenami <7box7@MSI>
2024-04-04 00:26:55 +0300

    alll

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  namespace :api do
+    resources :students, only: [:create, :show, :update, :delete]
+    resources :teachers, only: [:create, :show, :update, :delete]
+    resources :courses, only: [:create, :show, :update, :delete]
+    resources :participants, only: [:create, :show, :update, :delete]
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
