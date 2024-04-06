67e89ae38972bb21b8767428f420edb03e01155b
7box7 <60572459+7box7@users.noreply.github.com>
2024-04-06 13:55:20 +0300

    Delete spec directory

--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-    factory :user do
-      fio { "Учитель1" }
-      email { "email_teacher1@mail.ru" }
-      password { 'password1' }
-      teacher { true }
-    end
-  end
